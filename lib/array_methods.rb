2a883849571cf9ab41d9a68b94d3e23bbfcf501f
Mickey <micksheridan.24@gmail.com>
2019-01-28 18:07:30 -0500

    Submit

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  x = array.sort
+  x
 end
 
 def using_reverse(array)
-
+  x = array.reverse
+  x
 end
 
 def using_first(array)
-
+  x = array.first
+  x
 end
 
 def using_last(array)
-
+  x = array.last
+  x
 end
 
 def using_size(array)
-
+  x = array.size
+  x
 end
